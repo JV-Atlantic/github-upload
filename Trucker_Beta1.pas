797dae2e2434165741db0fb7ab730ed3a1e8f1a7
JaidonVProductions <76265888+JaidonVProductions@users.noreply.github.com>
2020-12-19 18:01:58 -0400

    Add files via upload
    Just a main pascal command line file

---
+++ Trucker_Beta1.pas
@@ -0,0 +1,9 @@
+Programme Trucker;
+
+var
+  Name:string;
+begin
+  WriteLn ("Hello. Please enter your name. It will be used to make your game experience more customisable. You can also enter anything you want. Whose stopping you from entering evil lavender pineapple pizza destroyer?");
+ReadLn "Name";
+  WriteLn ("Hello "Name"! Its nice to meet ya!");
+end.
